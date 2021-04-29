46d8c1d38c236f8d0b2e7efcd89068e4482b9c11
John Kim <johnkimmsterr@gmail.com>
2021-04-29 11:27:32 -0400

    trying switch statements

--- index.js
+++ index.js
@@ -1,53 +1,13 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
    
-    console.log(this.innerHTML);
-
+    switch(this.innerHTML){
+      case "w":
+        tom1.play()
+        break;
+    }
   });
 }
 
-document.addEventListener('keydown', function(e) {
-  if (e.key === 'w') {
-    document.getElementById('crash').play();
-  }
-});
-
-document.addEventListener('keydown', function(e) {
-  if (e.key === 'a') {
-    document.getElementById('kick').play();
-  }
-});
-
-document.addEventListener('keydown', function(e) {
-  if (e.key === 's') {
-    document.getElementById('snare').play();
-  }
-});
-
-document.addEventListener('keydown', function(e) {
-  if (e.key === 'd') {
-    document.getElementById('tom1').play();
-  }
-});
-
-document.addEventListener('keydown', function(e) {
-  if (e.key === 'j') {
-    document.getElementById('tom2').play();
-  }
-});
-
-document.addEventListener('keydown', function(e) {
-  if (e.key === 'k') {
-    document.getElementById('tom3').play();
-  }
-});
-
-document.addEventListener('keydown', function(e) {
-  if (e.key === 'l') {
-    document.getElementById('tom4').play();
-  }
-});
-
-
 var tom1 = new Audio('sounds/tom-1.mp3');
-tom1.play();
+// tom1.play();
