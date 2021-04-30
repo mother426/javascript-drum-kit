443a76cdc8dff6e6f1a5078f532520d255cdf280
John Kim <johnkimmsterr@gmail.com>
2021-04-30 10:32:04 -0400

    clicks and keydowns

--- index.js
+++ index.js
@@ -34,3 +34,38 @@ for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
     }
   });
 }
+
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      const tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      const tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      const tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      const tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "w":
+      const crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      const kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "s":
+      const snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    default: 
+      console.log(this.innerHTML);
+  }
+})
